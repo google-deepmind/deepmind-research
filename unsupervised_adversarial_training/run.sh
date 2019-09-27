60655a2797951197a3e8ba61225f2da697e9f7c5
Diego de Las Casas <diegolascasas@google.com>
2019-09-27 10:59:45 +0100

    Small fixes in license headers.
    PiperOrigin-RevId: 271534490

--- unsupervised_adversarial_training/run.sh
+++ unsupervised_adversarial_training/run.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 # Copyright 2019 Deepmind Technologies Limited.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,8 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-#!/bin/sh
 
 # Usage:
 # user@host:/path/to/deepmind_research$ unsupervised_adversarial_training/run.sh
