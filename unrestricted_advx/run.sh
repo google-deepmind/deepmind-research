ba40847091e0f081f8a1d4cfbc2560363cfc8e47
Chongli Qin <chongliqin@google.com>
2019-11-14 05:39:30 -0800

    Submission for the unrestricted adversarial contest.
    PiperOrigin-RevId: 280407381

---
+++ unrestricted_advx/run.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# Copyright 2019 Deepmind Technologies Limited.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Usage:
+# user@host:/path/to/deepmind_research$ unrestricted_advx/run.sh
+
+# Runs evaluation script
+source unrestricted_venv/bin/activate
+python3 -m unrestricted_advx.main
