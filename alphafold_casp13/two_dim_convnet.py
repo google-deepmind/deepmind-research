86a954eb29cd19c282e7bb751c5d070b6a1b0261
Augustin Zidek <augustinzidek@google.com>
2020-01-07 03:33:39 -0800

    Fix a wrong docstring.
    PiperOrigin-RevId: 288467233

--- alphafold_casp13/two_dim_convnet.py
+++ alphafold_casp13/two_dim_convnet.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""One dim convnet for angles prediction."""
+"""Two dimensional convolutional neural net layers."""
 
 from absl import logging
 import tensorflow.compat.v1 as tf
