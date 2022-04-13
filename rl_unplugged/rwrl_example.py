94702704c8cc99151b0c412fbe2a757fd0a3424a
Yilei Yang <yileiyang@google.com>
2022-04-13 08:31:38 +0100

    Remove unused comments related to Python 2 compatibility.
    PiperOrigin-RevId: 441399237

--- rl_unplugged/rwrl_example.py
+++ rl_unplugged/rwrl_example.py
@@ -1,4 +1,3 @@
-# Lint as: python3
 # pylint: disable=line-too-long
 # Copyright 2020 DeepMind Technologies Limited.
 #
--- tvt/pycolab/active_visual_match.py
+++ tvt/pycolab/active_visual_match.py
@@ -1,4 +1,3 @@
-# Lint as: python2, python3
 # pylint: disable=g-bad-file-header
 # Copyright 2019 DeepMind Technologies Limited. All Rights Reserved.
 #
