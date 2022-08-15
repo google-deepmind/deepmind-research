fd3e338b6f0837fdbf5a74691d738839c695b9bf
Songsong Wang <32472676+w169376@users.noreply.github.com>
2022-08-15 22:25:53 +0800

    Update run_pretrained_eval.sh

--- ogb_lsc/pcq/run_pretrained_eval.sh
+++ ogb_lsc/pcq/run_pretrained_eval.sh
@@ -44,29 +44,29 @@ CACHED_CONFORMERS_FILE=${CACHED_CONFORMERS_DIR}/smiles_to_conformers.pkl
 OUTPUT_DIR=${TASK_ROOT}/predictions/
 
 # First generate and cache the conformer feature data for the test split.
-mkdir -p ${CACHED_CONFORMERS_DIR}
+mkdir -p "${CACHED_CONFORMERS_DIR}"
 time python "${SCRIPT_DIR}"/generate_conformer_features.py \
   --splits=$SPLIT \
   --num_parallel_procs=32 \
-  --output_file=${CACHED_CONFORMERS_FILE}
+  --output_file="${CACHED_CONFORMERS_FILE}"
 
 seed=42
 # Share GPU between two runs by disabling XLA memory pre-allocation.
 export XLA_PYTHON_CLIENT_PREALLOCATE=false
-
+# shellcheck disable=SC2034
 for seed_group in 0 1; do
-  for k in `seq 0 9`; do
+  for k in $(seq 0 9); do
     # Conformer
     predictions_dir="${OUTPUT_DIR}/predictions/${SPLIT}/conformer/k${k}_seed${seed}"
     time python "${SCRIPT_DIR}"/experiment.py \
       --jaxline_mode="eval" \
       --config="${SCRIPT_DIR}"/config.py \
       --config.experiment_kwargs.config.evaluation.split=$SPLIT \
-      --config.restore_path=${MODELS_ROOT}/conformer/k${k}_seed${seed} \
-      --config.experiment_kwargs.config.predictions_dir=${predictions_dir} \
-      --config.experiment_kwargs.config.dataset_config.data_root=${DATA_ROOT} \
-      --config.experiment_kwargs.config.dataset_config.k_fold_split_id=$k \
-      --config.experiment_kwargs.config.dataset_config.cached_conformers_file=${CACHED_CONFORMERS_FILE} \
+      --config.restore_path="${MODELS_ROOT}"/conformer/k"${k}"_seed${seed} \
+      --config.experiment_kwargs.config.predictions_dir="${predictions_dir}" \
+      --config.experiment_kwargs.config.dataset_config.data_root="${DATA_ROOT}" \
+      --config.experiment_kwargs.config.dataset_config.k_fold_split_id="$k" \
+      --config.experiment_kwargs.config.dataset_config.cached_conformers_file="${CACHED_CONFORMERS_FILE}" \
       --config.experiment_kwargs.config.dataset_config.filter_in_or_out_samples_with_nans_in_conformers="out" \
       --config.experiment_kwargs.config.model.latent_size=256 \
       --config.experiment_kwargs.config.model.mlp_hidden_size=1024 \
@@ -80,11 +80,11 @@ for seed_group in 0 1; do
       --jaxline_mode="eval" \
       --config="${SCRIPT_DIR}"/config.py \
       --config.experiment_kwargs.config.evaluation.split=$SPLIT \
-      --config.restore_path=${MODELS_ROOT}/non_conformer/k${k}_seed${seed} \
-      --config.experiment_kwargs.config.predictions_dir=${predictions_dir} \
-      --config.experiment_kwargs.config.dataset_config.data_root=${DATA_ROOT} \
-      --config.experiment_kwargs.config.dataset_config.k_fold_split_id=$k \
-      --config.experiment_kwargs.config.dataset_config.cached_conformers_file=${CACHED_CONFORMERS_FILE} \
+      --config.restore_path="${MODELS_ROOT}"/non_conformer/k"${k}"_seed${seed} \
+      --config.experiment_kwargs.config.predictions_dir="${predictions_dir}" \
+      --config.experiment_kwargs.config.dataset_config.data_root="${DATA_ROOT}" \
+      --config.experiment_kwargs.config.dataset_config.k_fold_split_id="$k" \
+      --config.experiment_kwargs.config.dataset_config.cached_conformers_file="${CACHED_CONFORMERS_FILE}" \
       --config.experiment_kwargs.config.dataset_config.filter_in_or_out_samples_with_nans_in_conformers="in" \
       --config.experiment_kwargs.config.model.num_message_passing_steps=50 \
       --config.experiment_kwargs.config.model.add_relative_distance=false \
